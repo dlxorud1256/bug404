e76c62699398bcbaa9932476c3c0f06d9b582880
dlxorud125@gmail.com <dlxorud12567@gmail.com>
2025-12-24 16:49:01 +0900

    d

---
+++ src/api/api_method.py
@@ -0,0 +1,34 @@
+import requests
+
+def api_request(method, base_url, path, headers=None, **kwargs):
+    url = f"{base_url.rstrip('/')}{path}"
+    response = requests.request(
+        method, url, headers=headers, **kwargs
+    )
+
+    if not response.ok:
+        raise AssertionError(
+            f"""
+            ❌ API 요청 실패
+            METHOD: {method}
+            URL: {url}
+            STATUS: {response.status_code}
+            RESPONSE: {response.text}
+            """
+        )
+    return response
+
+def api_get(base_url, path, headers=None, **kwargs):
+    return api_request("GET", base_url, path, headers, **kwargs)
+
+
+def api_post(base_url, path, headers=None, **kwargs):
+    return api_request("POST", base_url, path, headers, **kwargs)
+
+
+def api_patch(base_url, path, headers=None, **kwargs):
+    return api_request("PATCH", base_url, path, headers, **kwargs)
+
+
+def api_delete(base_url, path, headers=None, **kwargs):
+    return api_request("DELETE", base_url, path, headers, **kwargs)
